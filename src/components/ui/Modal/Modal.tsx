92a72da61d9a2116fff1cc6fcf34cfdf8f8374f7
andrey <andrey.laptenok@fingers.by>
2023-08-18 15:16:53 +0200

    feat: react modal with portals

--- src/components/ui/Modal/Modal.tsx
+++ src/components/ui/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from 'react';
+import { createPortal } from 'react-dom';
 
 import { Icon } from 'components/ui/Icon/Icon';
 import { useEscapeKey } from 'hooks/useEscapeKey';
@@ -13,16 +14,18 @@ interface Props {
 
 export const Modal: FC<Props> = ({ children, isOpen, onClose }) => {
   useEscapeKey(onClose);
-  return (
-    <>
-      {isOpen && (
-        <div className={styles.modal__overlay}>
-          <div className={styles.modal}>
-            <Icon name="close" cursor="pointer" onClick={onClose} className={styles.modal__button} />
-            {children}
-          </div>
+
+  if (isOpen) {
+    return createPortal(
+      <div className={styles.modal__overlay}>
+        <div className={styles.modal}>
+          <Icon name="close" cursor="pointer" onClick={onClose} className={styles.modal__button} />
+          {children}
         </div>
-      )}
-    </>
-  );
+      </div>,
+      document.getElementById('modal-root'),
+    );
+  }
+
+  return null;
 };
